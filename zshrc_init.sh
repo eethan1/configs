67959c941b7801b5ad0dfa089c958b24be8137e3
eethan1 <call132call132@gmail.com>
2021-10-19 08:16:51 +0000

    upgrade zshrc

--- zshrc_init.sh
+++ zshrc_init.sh
@@ -6,7 +6,6 @@ cp ~/.zshrc ~/.zshrc.orig
 ln -sf $SCRIPTPATH/.zshrc ~/
 ln -sf $SCRIPTPATH/.zsh ~/
 cp $SCRIPTPATH/.zshrc.env ~/
-chsh -s $(which zsh)
 zshplgs=$ZSH/custom/plugins
 echo plugin path: $zshplgs
 git clone https://github.com/wting/autojump $zshplgs/autojump
@@ -16,4 +15,8 @@ cd ~
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $zshplgs/zsh-autosuggestions/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zshplgs/zsh-syntax-highlighting
 git clone https://github.com/paulirish/git-open.git $zshplgs/git-open
+vared -p 'chsh to zsh(y/n))?' -c _y
+if [ $_y = 'y' ] || [ $_y = 'Y' ]; then
+    chsh -s $(which zsh)
+fi
 
